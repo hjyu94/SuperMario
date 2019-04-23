ef231efd9081aafa0c61138d2456c00b77cd9f42
Administrator <Administrator@JUSIN-20180208X>
2019-04-23 15:00:56 +0900

    Define ALIVE로 바꿔주었음

---
+++ 슈퍼마리오/GrowMushroom.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Obj.h"
+class CGrowMushroom :
+	public CObj
+{
+public:
+	CGrowMushroom();
+	~CGrowMushroom();
+public:
+	// CObj을(를) 통해 상속됨
+	virtual void Initialize() override;
+	virtual int Update() override;
+	virtual void LateUpdate() override;
+	virtual void Render(HDC hDC) override;
+	virtual void Release() override;
+};
+
